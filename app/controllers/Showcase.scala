a327495592d434480b2e3f780919fbeb279c7933
epond <epond@users.noreply.github.com>
2014-12-06 13:39:32 +0000

    Implement showcase

--- app/controllers/Showcase.scala
+++ app/controllers/Showcase.scala
@@ -2,11 +2,40 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+
+import java.security.MessageDigest
+
+import scala.util.{Failure, Success, Try}
 
 object Showcase extends Controller {
+
+  implicit val stringInJsonFormat: Format[StringIn] = Json.format[StringIn]
+  implicit val md5OutJsonFormat: Format[MD5Out] = Json.format[MD5Out]
 
   def basic = Action { request =>
     Ok(request.body.asText.getOrElse("no request body").toUpperCase)
   }
 
+  def showcase = Action { request =>
+    val requestBody = request.body.asText.getOrElse("no request body")
+    val jsValueTry = Try(Json.parse(requestBody))
+    jsValueTry match {
+      case Success(jsValue) => {
+        jsValue.validate[StringIn] match {
+          case s: JsSuccess[StringIn] => {
+            val md5 = MessageDigest.getInstance("MD5").digest(s.get.input.getBytes("UTF-8"))
+              .map("%02X".format(_)).mkString
+            Ok(Json.toJson(MD5Out(md5, s.get.input)))
+          }
+          case e: JsError => BadRequest
+        }
+      }
+      case Failure(_) => BadRequest
+    }
+  }
+
 }
+
+case class StringIn(input: String)
+case class MD5Out(md5: String, originalString: String)
